279cff2c1391dc9e686dbdfae1fc8db272827719
Artem Belkov <bestK1ngArthur@aol.com>
2017-03-26 15:59:27 +0300

    Добавил лабу №4

--- 4 семестр/Системное программирование/Lab4/LAB.asm
+++ 4 семестр/Системное программирование/Lab4/LAB.asm
@@ -1,61 +1,214 @@
-; Лабораторная работа №4
+; Лабораторная работа №4 (+ дополнительные требования)
 MYCODE SEGMENT 'CODE'
 ASSUME CS:MYCODE
-LABTITLE db 'Лабораторная работа #4$'
+LABTITLE DB 'Лабораторная работа #4$'
+CURNTSMBL DB 'А'
+HEX_TABLE DB '0123456789ABCDEF'
 START:
 ; Загрузка сегментного регистра данных DS
-     push CS
-     pop  DS
+     push cs
+     pop  ds
 ; Очитка экрана
-     CALL CLRSCR
+     call CLRSCR
 ; Вывод заголовка
-     mov DX, offset LABTITLE
+     call CLRF
+     mov dx , offset LABTITLE
      call PUTSTR
      call CLRF
-; Вывод первого символа
-     mov DL , 'А'
+; Установка количества символов для вывода
+     mov cx , 10
+     call PRNTHEAD
+; Цикл вывода символов
+  LOOPCH:
+     mov dl , '│'
      call PUTCH
-; ..тут должен быть цикл с выводом символов..
+     mov dl, CURNTSMBL
+     inc CURNTSMBL
+     call PUTCH
+     mov dl, '│'
+     call PUTCH
+     call HEX
+     mov dl, 'h'
+     call PUTCH
+     mov dl , '│'
+     call PUTCH
+     mov dl, CURNTSMBL
+     inc CURNTSMBL
+     call PUTCH
+     mov dl, '│'
+     call PUTCH
+     call HEX
+     mov dl, 'h'
+     call PUTCH
+     mov dl , '│'
+     call PUTCH
+     call CLRF
+     cmp cx , 1 ; Если последний символ, то печатать хвост
+     je CONTINUE
+     call PRNTSEPRTR
+     loop LOOPCH
+  CONTINUE:
+     call PRNTTAIL
 ; Ожидание завершения программы
      call GETCH
 ; Вывод из программы
-     mov AL , 0
-     mov AH , 4CH
+     mov al , 0
+     mov ah , 4CH
      int 21H
+
 ; Процедуры:
+; -------------------------------
+; Перевод символа в код
+HEX PROC
+    mov dl , CURNTSMBL
+    lea bx , HEX_TABLE ; load data table.
+    mov al , dl
+    shr al , 4 ; leave high part only.
+    xlat ; Get hex digit.
+    mov ah , 0Eh ; Teletype sub-function.
+    int 10h
+    mov al , dl
+    and al , 0Fh ; leave low part only.
+    xlat ; Get hex digit.
+    mov ah , 0Eh ; Teletype sub-function.
+    int 10h
+    ret
+HEX ENDP
+; -------------------------------
 ; Перевод на новую строку
 CLRF PROC
-    mov DL , 10
+    mov dl , 10
     call PUTCH
-    mov DL , 13
+    mov dl , 13
     call PUTCH
     ret
 CLRF ENDP
+; -------------------------------
 ; Очистка экрана
 CLRSCR PROC
     call CLRF
-    mov  AH , 0H
-    mov  AL , 3H
+    mov  ah , 0H
+    mov  al , 3H
     int  10H
     ret
 CLRSCR ENDP
+; -------------------------------
 ; Вывод одного символа на экран
 PUTCH PROC
-    mov AH , 02H
-    int 021H
+    mov ah , 02H
+    int 21H
     ret
 PUTCH ENDP
+; -------------------------------
 ; Вывод строки
 PUTSTR PROC
-    mov AH , 09H
-    int 021H
+    mov ah , 09H
+    int 21H
     ret
 PUTSTR ENDP
+; -------------------------------
 ; Ввод символа с клавиатуры
 GETCH PROC
-    mov AH , 08H
-    int 021H
+    mov ah , 08H
+    int 21H
     ret
 GETCH ENDP
-MYCODE ENDS
+; -------------------------------
+; Печать шапки
+PRNTHEAD PROC
+    mov dl , '┌'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┬'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┬'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┬'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┐'
+    call PUTCH
+    call CLRF
+    ret
+PRNTHEAD ENDP
+; -------------------------------
+; Печать шапки
+PRNTSEPRTR PROC
+    mov dl , '├'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┼'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┼'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┼'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┤'
+    call PUTCH
+    call CLRF
+    ret
+PRNTSEPRTR ENDP
+; -------------------------------
+; Печать шапки
+PRNTTAIL PROC
+    mov dl , '└'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┴'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┴'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┴'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '─'
+    call PUTCH
+    mov dl , '┘'
+    call PUTCH
+    call CLRF
+    ret
+PRNTTAIL ENDP
+
 END START
+MYCODE ENDS
