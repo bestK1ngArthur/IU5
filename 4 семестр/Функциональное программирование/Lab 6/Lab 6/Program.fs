fa6c74453364de43ae151ee47427bd04e67244a9
Artem Belkov <bestK1ngArthur@aol.com>
2017-05-08 11:31:06 +0300

    Добавил парсер числовых выражений

--- 4 семестр/Функциональное программирование/Lab 6/Lab 6/Program.fs
+++ 4 семестр/Функциональное программирование/Lab 6/Lab 6/Program.fs
@@ -3,24 +3,66 @@
 open System
 open FParsec
 
+type Expr =
+        | Num of float
+        | Plus of Expr * Expr
+        | Minus of Expr * Expr
+
 [<EntryPoint>]
 let main argv =
     printfn "Лабораторная работа №6"
 
     // Задание 
-    printfn "Разработайте программу, которая осуществляет разбор текста с использованием библиотеки FParsec. Результатом разбора должны быть значения алгебраического типа."
+    printfn "Разработайте программу, которая осуществляет разбор текста с использованием библиотеки FParsec. Результатом разбора должны быть значения алгебраического типа.\n"
 
-    // Разбор текста в алгебаические типы
+    // Разбор числового выражения
+    // Создаем опережающее описание для парсера
+    let parseExpression, implementation = createParserForwardedToRef<Expr, unit>()
+    let parsePlus = tuple2 (parseExpression .>> pstring "+") parseExpression |>> Plus
+    let parseMinus = tuple2 (parseExpression .>> pstring "-") parseExpression |>> Minus
+    let parseOp = between (pstring "(") (pstring ")") (attempt parsePlus <|> parseMinus)
+    let parseNum = pfloat |>> Num
+    implementation := parseNum <|> parseOp
 
-    let test p str =
-        match run p str with
-        | Success(result, _, _) -> printfn "Success: %A" result
-        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
-    //разбор вещественного числа
-    test pfloat "333.333"
-    printfn "-------------------------------"
-    printfn "%A" (run pfloat "333.333")
-    test pfloat "qwerty333.333"
+    // Функция упрощения выражения
+    let rec EvalExpr(e:Expr):float =
+        match e with
+        | Num(num) -> num
+        | Plus(a,b) ->
+            let left =
+                match a with
+                | Num(num) -> num
+                | _ -> EvalExpr(a)
+            let right =
+                match b with
+                | Num(num) -> num
+                | _ -> EvalExpr(b)
+            let res1 = left + right
+            printfn "%f + %f = %f" left right res1
+            res1
+        | Minus(a,b) ->
+            let left =
+                match a with
+                | Num(num) -> num
+                | _ -> EvalExpr(a)
+            let right =
+                match b with
+                | Num(num) -> num
+                | _ -> EvalExpr(b)
+            let res2 = left - right
+            printfn "%f - %f = %f" left right res2
+            res2
+
+    printfn "(((3+4)+(1-2))+10)\n"
+    printfn "******parsing******\n"
+    let expr1Parser = run parseExpression "(((3+4)+(1-2))+10)"
+    printfn "%A" expr1Parser
+
+    match expr1Parser with
+    | Success(result, _, _) ->
+        let eval1 = EvalExpr(result)
+        printfn "Результат вычислений: %f" eval1
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
     //|> ignore - перенаправление потока с игнорирование результата вычисления
     Console.ReadLine() |> ignore
