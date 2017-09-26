bd52b85474568ae45c22a7757766d33da21b3d0d
Artem Belkov <bestK1ngArthur@aol.com>
2017-09-26 17:23:11 +0300

    Добавил 3 лабу по РИП

---
+++ 5 семестр/Разработка интернет-приложений/Lab3/base.py
@@ -0,0 +1,43 @@
+
+
+class VkRequest:
+  
+    # URL vk api
+    BASE_URL = None
+    # метод vk api
+    method = None
+    # GET, POST, ...
+    http_method = None
+
+    # Получение GET параметров запроса
+    def get_params(self):
+        return None
+
+    # Получение данных POST запроса
+    def get_json(self):
+        return None
+
+    # Получение HTTP заголовков
+    def get_headers(self):
+        return None
+
+    # Склейка url
+    def generate_url(self, method):
+        return '{0}{1}'.format(self.BASE_URL, method)
+
+    # Отправка запроса к VK API
+    def _get_data(self, method, http_method):
+        response = None
+        # todo выполнить запрос
+        return self.response_handler(response)
+
+    # Обработка ответа от VK API
+    def response_handler(self, response):
+        return response
+
+    # Запуск клиента
+    def execute(self):
+        return self._get_data(
+            self.method,
+            http_method=self.http_method
+        )
---
+++ 5 семестр/Разработка интернет-приложений/Lab3/main.py
@@ -0,0 +1,67 @@
+import datetime
+from get_friends import *
+from get_user import *
+
+
+def make_chart(friends):
+    chart = {}
+
+    for friend in friends:
+        date_birth = friend['bdate']
+        date = datetime.datetime.strptime(date_birth, '%d.%m.%Y')
+        years = int((datetime.datetime.now() - date).days/365.25)
+
+        if years in chart:
+            chart[years] += 1
+        else:
+            chart[years] = 1
+    return chart
+
+
+def draw_chart(chart):
+    keys = sorted(chart.keys())
+    for i in keys:
+        print(i, ":", chart[i]*'#')
+
+
+def load_with_id(user_id):
+    final_friends = []
+    
+    request = GetFriends(user_id)
+    try:
+        friends = request.execute().json()['response']
+    except KeyError:
+        print('Incorrect id or username')
+    else:
+        for friend in friends:
+            if 'bdate' in friend:
+                if len(friend['bdate'].split('.')) > 2:
+                    final_friends.append(friend)
+
+        chart = make_chart(final_friends)
+        draw_chart(chart)
+
+
+def load_with_username(username):
+
+    request = GetUser(username)
+    user = request.execute()
+    try:
+        user = user.json()['response']
+    except KeyError:
+        print('Incorrect id or username')
+    else:
+        user_id = user[0]['uid']
+        load_with_id(user_id)
+    
+
+if __name__ == '__main__':
+    user_input = input("Введите id: ")
+
+    if user_input.isdigit():
+        id = int(user_input)
+        load_with_id(id)
+    else:
+        username = user_input
+        load_with_username(username)
+        
