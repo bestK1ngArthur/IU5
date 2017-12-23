1121f03317301530c51fe14aaefed3206668fd4f
Artem Belkov <bestK1ngArthur@aol.com>
2017-12-23 21:20:04 +0300

    Добавил локальные картинки и немного причесал код

--- 5 семестр/Разработка интернет-приложений/Homework/Homework/settings.py
+++ 5 семестр/Разработка интернет-приложений/Homework/Homework/settings.py
@@ -132,4 +132,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
--- 5 семестр/Разработка интернет-приложений/Homework/lab_app/models.py
+++ 5 семестр/Разработка интернет-приложений/Homework/lab_app/models.py
@@ -1,24 +1,30 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
-
-
 # -*- coding: utf-8 -*-
-# Create your models here.
+
 
 class Product(models.Model):
-    name = models.CharField(
-        max_length=255,
-    )
-    description = models.CharField(
-        max_length=1000,
-    )
-    seller = models.CharField(
-        max_length=255,
-    )
-    image_url = models.URLField(
-        max_length=255
-    )
+    class Meta:
+        db_table = 'lab_app_product'
 
+    # Название товара
+    name = models.CharField(max_length=255)
+
+    # Описание товара
+    description = models.CharField(max_length=1000)
+
+    # Продавец
+    seller = models.CharField(max_length=255)
+
+    # Ссылка на картинку товара
+    image_url = models.URLField(max_length=255)
+
+    # Полный путь до картинки товара
+    def image_path(self):
+        return settings.STATIC_URL + self.image_url
+
+    # Короткое описание товара
     def short_description(self):
         return self.description[:126]
 
@@ -31,18 +37,22 @@ class Product(models.Model):
 
 
 class Review(models.Model):
-    user_id = models.IntegerField()
-    product_id = models.IntegerField()
+    class Meta:
+        db_table = 'lab_app_review'
+
+    # Пользователь, который оставил отзыв
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Товар, под которым оставлен отзыв
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    # Текст отзыва
     description = models.CharField(
         max_length=500,
     )
 
-    def username(self):
-        user = User.objects.get(id=self.user_id)
-        return user.username
-
     def __str__(self):
         return ' '.join([
             'review from user ',
-            str(self.user_id),
+            str(self.user.username),
         ])
