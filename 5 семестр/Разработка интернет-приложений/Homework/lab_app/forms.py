81be3dd22f371da69794a779072e16897c394864
Artem Belkov <bestK1ngArthur@aol.com>
2017-12-24 12:54:15 +0300

    Подразобрал немного формы

--- 5 семестр/Разработка интернет-приложений/Homework/lab_app/forms.py
+++ 5 семестр/Разработка интернет-приложений/Homework/lab_app/forms.py
@@ -1,8 +1,45 @@
 from django import forms
-from lab_app.models import Review
+from lab_app.models import Review, Product
+from django.contrib.auth.models import User
+
+# TODO: Перенести создания форм в джанговские классы
 
 
-class PostForm(forms.ModelForm):
+# Форма регистрации
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        widgets = {
+            'text': forms.TextInput(
+                attrs={'id': 'post-text', 'required': True, 'placeholder': 'Say something...'}
+            ),
+        }
+
+
+# Форма авторизации
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        widgets = {
+            'text': forms.TextInput(
+                attrs={'id': 'post-text', 'required': True, 'placeholder': 'Say something...'}
+            ),
+        }
+
+
+# Форма добавления продукта
+class AddProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        widgets = {
+            'text': forms.TextInput(
+                attrs={'id': 'post-text', 'required': True, 'placeholder': 'Say something...'}
+            ),
+        }
+
+
+# Форма создания отзыва
+class PostReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['text']
@@ -11,3 +48,4 @@ class PostForm(forms.ModelForm):
                 attrs={'id': 'post-text', 'required': True, 'placeholder': 'Say something...'}
             ),
         }
+
